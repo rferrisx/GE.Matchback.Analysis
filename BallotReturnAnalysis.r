b1a7a681c53382bb6ec8988937b2ea3815c26784
rferrisx <rferrisx@gmail.com>
2020-11-02 20:35:41 -0800

    Update BallotReturnAnalysis.r
    updated 11/2/2020

--- BallotReturnAnalysis.r
+++ BallotReturnAnalysis.r
@@ -1,7 +1,5 @@
 # R 4.02
-# install.packages("x") below if needed
-# rdata.table 1.13.1 Code for analyzing WA State matchbacks:
-# https://www.sos.wa.gov/elections/research/election-results-and-voters-pamphlets.aspx
+#install.packages("x") below if needed
 
 library(data.table)
 library(lubridate)
@@ -11,15 +9,13 @@ fsum <- function(x) {sum(x,na.rm=TRUE)}
 # apply and receive current VRDB from https://www.sos.wa.gov/elections/vrdb/default.aspx
 # setwd("D:/Politics/General.2020/")
 # OctVDB <- fread("202010_VRDB_Extract.txt",fill=TRUE)
+# setkey(OctVDB,StateVoterID)
 # OctVDB <- fread("202010_VRDB_Extract.txt")
-# source("D:/Politics/General.2020/BallotReturnStats/BallotReturnAnalysis.r",print.eval=TRUE)
-
-# replace numberical variables in this sample analysis text as needed...
-print("For 10/27/2020, over 2.56M ballots returned with ~32K challenged ballots in WA. Almost 19K of these ballots are signature matching challenges. Another ~5K are missing signatures. Voters will should visit https://voter.votewa.gov and check their ballot status. For ballot status other than "Accepted", voters may want to contact or visit their county election department websites found at https://www.sos.wa.gov/elections/auditors .
-King county leads the the top five counties in turnout percentage at 53% based on October 7 VRDB registrants.  However King's current turnout percentage is actually 56% based on the latest registration data (Please See https://www.sos.wa.gov/elections/research/ballot-return-statistics.aspx ) This would suggest increases from October first time voter registration and/or reactivations.    8.7% of all returned ballots were by voters who last voted in 2018 or earlier. 174,176 of all returned ballots were by voters whose 'LastVoted' field is "NA" suggesting an increasing number of 'first time' voters in this election. General election return ballot data can be found: https://www.sos.wa.gov/elections/research/2020-general-election.aspx .  Based on past elections performances, a reasonable prediction for total votes would be 250K for the next two days then ~500K/day for the next 5 days. However, such a prediction would result in 5.25M returned ballots. Current total voters for this election are 4,866,924, although registrations will continue through election. Regardless of this comment, turnout will likely be record setting for every county."
+# # source("D:/Politics/General.2020/BallotReturnStats/BallotReturnAnalysis.r",print.eval=TRUE)
 
 # unzip all the files you want row bound .... to this directory
-setwd("D:/Politics/General.2020/BallotReturnStats/10.27.2020")      
+setwd("D:/Politics/General.2020/BallotReturnStats/11.02.2020")
+
 l1 <- as.data.table({})
 for(i in dir()) {l1 <- rbind(l1,fread(i),fill=TRUE)}
 colnames(l1) <- l1[,gsub(" ","",names(.SD))]
@@ -41,6 +37,7 @@ lbs <- l1[,.(County),by="BallotStatus"][
 ,.SD[,.(RejectPCT=round(Rejected/(Accepted+Received),4) * 100)],
 .(County,Accepted,Received,Rejected)]
 lbs[]
+lbs[order(-Accepted)]
 
 #Challenge Return
 print("Some Top Challenge Returns by County:")
@@ -53,8 +50,11 @@ lcr[]
 
 # Top Counties for Turnout Percent 
 # needs OctVDB
-# Your latest VDB will never be up to date with Same Day Registration. A voter list is never really static except for some mu second point in time...
-print("Merges October 7th votedb with current ballot status list. Thus later registrations are not part of the calculus here")
+print("Merges October 7th votedb with current ballot status list. Thus later registrations are not part of the calculus here.")	
+print("Please see https://www.sos.wa.gov/elections/research/ballot-return-statistics.aspx for up to date percentages and turnout.")
+OctVDB$StateVoterID <- as.integer(OctVDB$StateVoterID)
+l1$VoterID <- as.integer(l1$VoterID)
+OctVDB$StateVoterID <- as.integer(OctVDB$StateVoterID)
 t1 <- merge(l1[,.(VoterID,BallotStatus)],OctVDB[,.(StateVoterID,CountyCode,PrecinctCode)],all.x=TRUE,by.x="VoterID",by.y="StateVoterID")[BallotStatus !="",][,.N,keyby=.(CountyCode,PrecinctCode)]
 t2 <- OctVDB[,.N,.(CountyCode,PrecinctCode)]
 t3 <- merge(t1,t2,all=TRUE,by=c("CountyCode","PrecinctCode"))
@@ -108,7 +108,10 @@ t(ssum)
 
 # Location Top 20s
 print("Top 20 aggregate counts for County,City,Zip,c(City,Zip),c(County,Zip).")
-cbind(l1[,.N,.(County)][order(-N)][1:20],l1[,.N,.(City)][order(-N)][1:20], l1[,.N,.(Zip)][order(-N)][1:20],l1[,.N,.(City,Zip)][order(-N)][1:20],l1[,.N,.(County,Zip)][order(-N)][1:20])
-
-
+cbind(
+l1[,.N,.(County)][order(-N)][1:20],
+l1[,.N,.(City)][order(-N)][1:20],
+l1[,.N,.(Zip)][order(-N)][1:20],
+l1[,.N,.(City,Zip)][order(-N)][1:20],
+l1[,.N,.(County,Zip)][order(-N)][1:20])
 
